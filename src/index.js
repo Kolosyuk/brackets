c15d0a55b93aeef1340779c3c20bd0f51e12713c
Kolosyuk <crplutt@gmail.com>
2020-10-06 15:01:36 +0300

    init: brackets task. solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.join('').split(',').join('');
+  let arrStr = str.split('');
+  for (let item = 0; item < arrStr.length; item++) {
+    if (arrStr[item + 1] === config[config.indexOf(arrStr[item]) + 1]) {
+      arrStr.splice(item, 2)
+      item = -1;
+      if (arrStr.length === 0) return true
+      continue
+    }
+    continue
+  }
+  return false
 }
